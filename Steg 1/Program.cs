5da37201bb6d28789828f9a495015e3023c06e8c
Elias Saarenala <elias.saarenala@elev.ga.ntig.se>
2024-11-05 11:02:17 +0100

    sdryjeryu

---
+++ Steg 1/Program.cs
@@ -0,0 +1,22 @@
+﻿if (6 >=3)
+{
+    Console.WriteLine("Hello, World!");
+}
+
+
+string Lösenord = Console.ReadLine();
+string Användarnamn = Console.ReadLine();
+if (Användarnamn=="Noname" && Lösenord=="Nopass")
+{
+    Console.WriteLine("Welcome");
+}
+else{
+    Console.WriteLine("Wrong username or password");
+}
+
+for (int i = 0; i < 32; i++)
+{
+    Console.WriteLine("Hello, World");
+}
+
+Console.ReadLine();
