fa314e72651c9b1f4aefee47ed6c32a93970ed07
Elias Saarenala <elias.saarenala@elev.ga.ntig.se>
2024-11-12 10:20:53 +0100

    övning 1 klar

--- Steg 1/Program.cs
+++ Steg 1/Program.cs
@@ -4,19 +4,44 @@
 }
 
 
-string Lösenord = Console.ReadLine();
-string Användarnamn = Console.ReadLine();
-if (Användarnamn=="Noname" && Lösenord=="Nopass")
+string Lösenord = "";
+string Användarnamn ="";
+while(!(Användarnamn=="kalleanka" && Lösenord=="12345"))
 {
-    Console.WriteLine("Welcome");
+Användarnamn = Console.ReadLine();
+Lösenord = Console.ReadLine();
+    if (Användarnamn=="kalleanka" && Lösenord=="12345")
+        {
+            Console.WriteLine("Welcome");
+        }
+    else
+        {
+            Console.WriteLine("Wrong username or password");
+        }
 }
-else{
-    Console.WriteLine("Wrong username or password");
-}
+
 
 for (int i = 0; i < 32; i++)
 {
     Console.WriteLine("Hello, World");
+}
+string input;
+int Numer;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Skriv ett numer");
+    input=Console.ReadLine();
+    while (!int.TryParse(input, out Numer))
+    {
+
+        Console.WriteLine(" Skriv ett numer!");
+        input=Console.ReadLine();
+    }
+
+    if (Numer>5)
+    {
+        Console.WriteLine("högre än 5!");
+    }
 }
 
 Console.ReadLine();
